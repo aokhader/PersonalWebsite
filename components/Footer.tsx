cdd6b670f75c728aaf9f15a62bb4f491be8569d8
AbdulazizKhader <akhader@ucsd.edu>
2024-09-24 01:44:49 -0700

    fixed some bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
-    <footer className="w-full py-20 pb-10" id="contact">
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
         <div className="w-full absolute left-0 -bottom-72 min-h-96 overflow-hidden">
             <img 
                 src="/footer-grid.svg"
