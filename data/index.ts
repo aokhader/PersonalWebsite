8abcfaea02b28afb8e47bb72104a5f2571b20e20
AbdulazizKhader <akhader@ucsd.edu>
2024-09-27 19:44:43 -0700

    Commented out testimonials for now

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  // { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a conversational AI Assistant",
+    title: "Currently building a conversational AI Assistant to run on the web",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
